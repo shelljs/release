f9f157b4f7ef727bef91f83b6319c59791b85693
Nate Fischer <ntfschr@google.com>
2025-03-12 21:21:31 -0700

    chore: update shelljs and use shell.cmd()

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var minimist = require('minimist');
 var GIT_MAIN_BRANCHES = [ 'main', 'master' ];
 function findMainBranch() {
   for (branch of GIT_MAIN_BRANCHES) {
-    var branchExists = shell.exec('git branch --list ' + branch, { silent: true }).trim() != '';
+    var branchExists = shell.cmd('git', 'branch', '--list', branch).trim() != '';
     if (branchExists) {
       return branch;
     }
@@ -54,7 +54,7 @@ function usage() {
 }
 
 function gitBranch() {
-  var output = shell.exec('git rev-parse --abbrev-ref HEAD', { silent: true });
+  var output = shell.cmd('git', 'rev-parse', '--abbrev-ref', 'HEAD');
   if (output.code) {
     throw new Error('Unable to fetch git branch');
   }
@@ -86,7 +86,7 @@ function run(argv) {
   try {
     shell.echo('Publishing new ' + version + ' version');
     shell.echo('');
-    shell.exec('npm version ' + version);
+    shell.cmd('npm', 'version', version);
   } catch (e) {
     shell.echo('');
     shell.echo('Unable to bump version, is your repo clean?');
@@ -94,7 +94,7 @@ function run(argv) {
   }
 
   try {
-    var npmVersions = shell.exec('npm --version').trim().split('.');
+    var npmVersions = shell.cmd('npm', '--version').trim().split('.');
     if (npmVersions[0] < 6 && argv.otp) {
       // I'm not sure about npm v5, but I've verified the --otp switch is not
       // documented for npm v4 and below.
@@ -102,11 +102,11 @@ function run(argv) {
           'The --otp switch only supports npm >= v6. Upgrade your node/nvm.');
     }
     // 'npm publish' will internally call both 'git commit' and 'git tag'
-    var publishCmd = 'npm publish';
+    var publishCmd = ['npm', 'publish'];
     if (argv.otp) {
-      publishCmd += ' --otp=' + argv.otp;
+      publishCmd.push('--otp=' + argv.otp);
     }
-    shell.exec(publishCmd);
+    shell.cmd(...publishCmd);
   } catch (e) {
     shell.config.fatal = false;
     shell.echo('');
@@ -118,13 +118,13 @@ function run(argv) {
 
     // Delete the tag and undo the commit
     shell.echo('Removing git tag...');
-    var cleanTag = shell.exec('git tag -d ' + tagName);
+    var cleanTag = shell.cmd('git', 'tag', '-d', tagName);
     shell.echo('Removing git commit...');
-    var cleanCommit = shell.exec('git reset --hard HEAD~1');
+    var cleanCommit = shell.cmd('git', 'reset', '--hard', 'HEAD~1');
     if (cleanTag.code === 0 && cleanCommit.code === 0) {
       shell.echo(chalk.white.bold('Successfully cleaned up commit and tag'));
     }
-    var npm_user = shell.exec('npm whoami', { silent: true }).trimRight();
+    var npm_user = shell.cmd('npm', 'whoami').trimRight();
     if (npm_user.toString() === '') {
       shell.config.silent = false;
       shell.echo('');
@@ -133,10 +133,11 @@ function run(argv) {
       shell.exit(1);
     }
     shell.config.silent = true;
-    var is_collaborator = shell.exec('npm access ls-collaborators')
+    var is_collaborator = shell.cmd('npm', 'access', 'ls-collaborators')
                                .grep('.*' + npm_user + '.*:.*write.*')
                                .trimRight();
-    var is_owner = shell.exec('npm owner ls').grep('.*' + npm_user + ' <.*')
+    var is_owner = shell.cmd('npm', 'owner', 'ls')
+                        .grep('.*' + npm_user + ' <.*')
                         .trimRight();
     if (is_collaborator + is_owner === '') {
       // Neither collaborator nor owner
@@ -144,7 +145,7 @@ function run(argv) {
       shell.echo(chalk.yellow.bold(
           npm_user + ' does not have NPM write access. Request access from one of these fine folk:'));
       shell.echo('');
-      shell.exec('npm owner ls');
+      shell.cmd('npm', 'owner', 'ls');
       shell.exit(1);
     }
 
@@ -155,10 +156,13 @@ function run(argv) {
 
   shell.config.silent = false;
   try {
-    shell.exec('git push origin ' + releaseBranch);
+    var result;
+    result = shell.cmd('git', 'push', 'origin', releaseBranch);
+    console.log(result.stdout);
     var newVersion = require(path.resolve('.', 'package.json')).version;
     var tagName = 'v' + newVersion;
-    shell.exec('git push origin refs/tags/' + tagName);
+    result = shell.cmd('git', 'push', 'origin', 'refs/tags/' + tagName);
+    console.log(result.stdout);
   } catch (e) {
     shell.echo('');
     shell.echo('Version has been released, but commit/tag could not be pushed.');
